ad373b84171175429f628741ee8801e53596ee5b
mattlevine22 <mattlevine22@gmail.com>
2021-09-14 14:02:05 -0700

    commenting L63 example

--- example_3d_l63_v2.py
+++ example_3d_l63_v2.py
@@ -6,7 +6,10 @@ from odelibrary import L63
 
 fig_path = 'outputs/example_3d_l63_v2'
 
-# This is for x 2D
+##### This example is designed to use trajectory data to estimate 28*x, -1*y under missing "-xz" term.
+
+
+
 Dx = 3 # input dimension
 
 # true function
@@ -19,6 +22,7 @@ def fdag(x, rho=28):
 	return foo
 fdag_str = '28*x - y - xz' # target function
 
+# constant library term
 def I_i(x_input):
 	# x_input: N x Dx
 	# output: N x 1 (constant)
@@ -26,6 +30,7 @@ def I_i(x_input):
 	foo = np.ones((N,1))
 	return foo
 
+# linear library term
 def X_i(x_input, i):
 	# x_input: N x Dx
 	# output: N x 1
@@ -33,8 +38,10 @@ def X_i(x_input, i):
 	foo = x_input[:,i].reshape(N,1)
 	return foo
 
+# build list of library functions
 lib_list = [lambda x: X_i(x, 0), lambda x: X_i(x, 1)]
 
+# build list of library function names as strings for printing
 lib_list_str = []
 lib_list_str += ['x_{}'.format(i) for i in range(2)]
 
@@ -46,25 +53,33 @@ lam_rf = 0 # regularization (L2) on the random feature function
 zero_thresh = 0 # inner-product threshold for discarding nearly-zero orthogonal features in Gram-Schmidt
 
 # hyperparameters for variance of random feature function parameters w,b: tanh(wx + b)
-Dr = 100 # number of random features
+Dr = 20 # number of random features
 fac_w = 0.1
 fac_b = 2*np.pi
 
 # coarsely sample data from the target function
-data_step = 0.1
-x_min = -10
-x_max = 10
+# data_step = 0.1
+# x_min = -10
+# x_max = 10
 
-dt = 0.01
-T = 100
+delta_t = 0.01
+t_data = 100 # length of training trajectory in model time units
+t_transient = 100 # burn-in length for data generation
+rng_seed = 0 # =None for random
 ode = L63()
-X, Xdot = ode.generate_data(delta_t=0.01, t_transient=100, t_data=100)
+X, Xdot = ode.generate_data(delta_t=delta_t, t_transient=t_transient, t_data=t_data, rng_seed=rng_seed)
 integration_ranges = [(np.min(X[:,i]), np.max(X[:,i])) for i in range(Dx)]
 
-y = Xdot[:,1,None]
+# set output data
+y = Xdot[:,1,None] # output data
+
+# rescale inputs
+print('Target is 28 x1 - 1 x2') # computed with standard OLS and data_step=0.0001
+sd_X = np.std(X,axis=0)
+X = X / sd_X
+print('Target (/sd) is {} x1 - {} x2'.format(28*sd_X[0], sd_X[1]))
 
 
-print('Target is 28 x1 + 1 x2') # computed with standard OLS and data_step=0.0001
 
 settings = {'lib_list': lib_list,
 			'lib_list_str': lib_list_str,
@@ -82,9 +97,9 @@ settings = {'lib_list': lib_list,
 			'zero_thresh': zero_thresh,
 			'fac_w': fac_w,
 			'fac_b': fac_b,
-			'data_step': data_step,
-			'x_min': x_min,
-			'x_max': x_max,
+			# 'data_step': data_step,
+			# 'x_min': x_min,
+			# 'x_max': x_max,
 			'Dx': Dx
 			}
 
